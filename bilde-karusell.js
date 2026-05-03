b92eac61afebca588e6d563a98f33bf2770bb355
Cpgamer <kajsarost06@outlook.com>
2026-05-03 12:48:30 +0200

    forsok pa a endre fargen pa knapper til bilde-karusellen

---
+++ bilde-karusell.js
@@ -0,0 +1,23 @@
+import "swiper/css";
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+
+import "./css/endre-farger-knapper.css";
+new Swiper('.card-wrapper', {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    // dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  
+});
