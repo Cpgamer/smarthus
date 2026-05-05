45e3c48c14c399043b4049ebb3389f5d447630cf
Cpgamer <kajsarost06@outlook.com>
2026-05-05 08:59:47 +0200

    kommentert ut imports på toppen av filen

--- bilde-karusell.js
+++ bilde-karusell.js
@@ -1,8 +1,8 @@
-import "swiper/css";
-import "swiper/css/navigation"
-import "swiper/css/pagination"
+// import "swiper/css";
+// import "swiper/css/navigation"
+// import "swiper/css/pagination"
 
-import "./css/endre-farger-knapper.css";
+// import "./css/endre-farger-knapper.css";
 new Swiper('.card-wrapper', {
   loop: true,
 
